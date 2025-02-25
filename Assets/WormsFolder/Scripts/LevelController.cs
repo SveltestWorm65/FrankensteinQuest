dab4431b14bd158e1fe2162b03b7ec5732ab0504
ISAIAH BRITT <wormgamedevelopment@gmail.com>
2025-02-25 08:59:39 -0800

    Level controller script made

---
+++ Assets/WormsFolder/Scripts/LevelController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelController : MonoBehaviour
+{
+    public string nextLevel;
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
